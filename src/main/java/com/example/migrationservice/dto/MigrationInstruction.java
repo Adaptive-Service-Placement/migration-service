3a521843fbfb138761b746a88546cb6a040378e7
fsix <six@campus.tu-berlin.de>
2023-03-06 19:48:36 +0100

    fixed MigrationInstruction object serialization, adjacencyMap uses string service id as key to avoid serialization error

--- src/main/java/com/example/migrationservice/dto/MigrationInstruction.java
+++ src/main/java/com/example/migrationservice/dto/MigrationInstruction.java
@@ -2,12 +2,20 @@ package com.example.migrationservice.dto;
 
 import com.example.migrationservice.Connection;
 import com.example.migrationservice.Service;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class MigrationInstruction {
     private List<Set<Service>> groups;
-    private Map<Service, List<Connection>> adjacencyMap;
+    private Map<String, List<Connection>> adjacencyMap;
 
     public List<Set<Service>> getGroups() {
         if (groups == null) {
@@ -16,7 +24,7 @@ public class MigrationInstruction {
         return groups;
     }
 
-    public Map<Service, List<Connection>> getAdjacencyMap() {
+    public Map<String, List<Connection>> getAdjacencyMap() {
         if (adjacencyMap == null) {
             adjacencyMap = new HashMap<>();
         }
